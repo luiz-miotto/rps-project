a074828fda0f7cdae6c5e358b5787f7862300633
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2022-03-20 12:30:16 -0400

    added capitalization logic to playerInput string

--- script.js
+++ script.js
@@ -3,34 +3,51 @@
 
 function computerPlay(){ 
     let rpsOptions = [
-    'rock',
-    'paper',
-    'scissors'
+    'Rock',
+    'Paper',
+    'Scissors'
     ];
 
     return computerMath = rpsOptions[Math.floor(Math.random()*rpsOptions.length)];
+    
 }
 
 
-let playerSelection ="scissors";
+
+
+
+
+function playerInputCapitalizer(playerInput){
+    let string = playerInput;  
+    let first = string.substring(0,1); /// this extracts just the first letter
+    let capitalized = first.toUpperCase();  /// this uppercases it
+    let rest = string.substring(1); /// this extracts the rest of the string
+    let undercase = rest.toLowerCase(); /// this makes the rest lowercase
+    return final = capitalized + undercase;
+    
+    }
+
+let playerSelection = playerInputCapitalizer("paper");
 let computerSelection = computerPlay();
 
-if (playerSelection==="rock" && computerSelection==="scissors"){
+if (playerSelection==="Rock" && computerSelection==="Scissors"){
     console.log(`${playerSelection} beats ${computerSelection}. You win!`)
-} else if ( playerSelection==="rock" && computerSelection==="rock"){
+} else if ( playerSelection==="Rock" && computerSelection==="Rock"){
     console.log(`${playerSelection} ties ${computerSelection}. You tied!`)
-} else if ( playerSelection==="rock" &&  computerSelection==="paper"){
+} else if ( playerSelection==="Rock" &&  computerSelection==="Paper"){
     console.log(`${computerSelection} beats ${playerSelection}. You lose!`)
-} else if ( playerSelection==="paper" && computerSelection==="rock"){
+} else if ( playerSelection==="Paper" && computerSelection==="Rock"){
     console.log(`${playerSelection} beats ${computerSelection}. You win!`)
-} else if (playerSelection==="paper" && computerSelection==="paper"){
+} else if (playerSelection==="Paper" && computerSelection==="Paper"){
     console.log(`${playerSelection} ties with ${computerSelection}. You tied!`)
-} else if (playerSelection==="paper" && computerSelection==="scissors"){
+} else if (playerSelection==="Paper" && computerSelection==="Scissors"){
     console.log(`${computerSelection} beats ${playerSelection}. You lose!`)
-} else if (playerSelection==="scissors" && computerSelection==="paper"){
+} else if (playerSelection==="Scissors" && computerSelection==="Paper"){
     console.log(`${playerSelection} beats ${computerSelection}. You win!`)
-}  else if (playerSelection==="scissors" && computerSelection==="scissors"){
+}  else if (playerSelection==="Scissors" && computerSelection==="Scissors"){
     console.log(`${playerSelection} ties with ${computerSelection}. You tied!`)
-}  else if (playerSelection==="scissors" && computerSelection==="rock"){
+}  else if (playerSelection==="Scissors" && computerSelection==="Rock"){
      console.log(`${computerSelection} beats ${playerSelection}. You lose!`)
 };
+
+
